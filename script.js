757849a8e9dd1772e4c1fd6ae72e2877f11cd7f0
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-31 15:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let output=document.getElementById("output");
+async function call{
+	return new promise((resolve,reject)=>{
+		resolve("Hello, world!");
+	},1000).then((data)=>{
+		return data;
+	})
+}
+     output.textContent=call();
