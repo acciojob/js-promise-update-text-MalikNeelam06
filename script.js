ddd70f2f9c6f3a387f14c2ffafcb9a1b274ecdc5
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-09-01 11:38:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-//your JS code here. If required.
-let output=document.getElementById("output");
-   let prom= async function call{
-	return new promise((resolve,reject)=>{
-		resolve("Hello, world!");
-	},1000)
+// //your JS code here. If required.
+// let output=document.getElementById("output");
+//    let prom= async function call{
+// 	return new promise((resolve,reject)=>{
+// 		resolve("Hello, world!");
+// 	},1000)
+// }
+// prom.then((data)=>data);
+// call();
+//      output.textContent=prom;
+let output = document.getElementById("output");
+
+function createPromise() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Hello, world!");
+        }, 1000);
+    });
 }
-prom.then((data)=>data);
-call();
-     output.textContent=prom;
+
+createPromise().then((data) => {
+    output.textContent = data;
+});
